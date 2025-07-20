e8712152d215ffc34fd1118a6f942ec2a29e0190
Hossein <hosseinbehvandi80@gmail.com>
2025-07-21 02:59:16 +0330

    a little styling for button and icon looks better

--- src/Button.js
+++ src/Button.js
@@ -10,7 +10,7 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = className("m-2 px-3 py-1.5 border", {
+  const classes = className("flex items-center m-2 px-3 py-1.5 border", {
     "border-blue-500 bg-blue-500 text-white": primary && !outline,
     "border-gray-900 bg-gray-900 text-white": secondary && !outline,
     "border-green-500 bg-green-500 text-white": success && !outline,
