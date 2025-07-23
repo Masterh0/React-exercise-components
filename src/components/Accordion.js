de7e6de286c3c30b752250ad9f663fa03aa3b48b
Hossein <hosseinbehvandi80@gmail.com>
2025-07-24 01:28:11 +0330

    add icon down and left to lable div

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { FaAngleDown ,FaAngleLeft  } from "react-icons/fa";
 function Accordion({items}){
-    const [expendedIndex , setExpendedIndex] = useState(2)
+    const [expendedIndex , setExpendedIndex] = useState(5)
     const renderedAccordion= items.map((item , index)=>{
         const isExpended = index === expendedIndex
+        const icon = <span>
+            {isExpended ? <FaAngleDown/> : <FaAngleLeft />}
+        </span>
         return <div key={item.id}>
-            <div onClick={()=>setExpendedIndex(index)} className="m-2">{item.lable}</div>
+            <div onClick={()=>setExpendedIndex(index)} className="m-2 cursor-pointer">
+                {item.lable}
+                {icon}
+                </div>
             {isExpended && <div>{item.content}</div>}
         </div>
     })
