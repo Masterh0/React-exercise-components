71de6b91071d66b88e0a4763bf3abce6a7696bf8
Hossein <hosseinbehvandi80@gmail.com>
2025-07-23 22:43:56 +0330

    render all prop items with map and pass key to it

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,13 @@
 function Accordion({items}){
-    return <div/>
+    const renderedAccordion= items.map((item)=>{
+        return <div key={item.id}>
+            <div className="m-2">{item.lable}</div>
+            <div>{item.content}</div>
+        </div>
+    })
+    return <div>
+        {renderedAccordion}
+    </div>
 }
 
 export default Accordion;
