6b339f6dcfb11667ad51007f941ee31a08342105
Hossein <hosseinbehvandi80@gmail.com>
2025-07-23 22:40:22 +0330

    create prop for accordion items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import Accordion from "./components/Accordion";
 function App(){
+    const items=[
+        {
+            id:"gjwuer9f",
+            lable:"can use react in this project",
+            content:"yes you can use react as you want in any project,yes you can use react as you want in any project,yes you can use react as you want in any project,yes you can use react as you want in any project,"
+        },
+        {
+            id:"qwepigherh9g",
+            lable:"can use css in this project",
+            content:"yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,"
+        },
+        {
+            id:"ewrg;pkjhweg",
+            lable:"can use js in this project",
+            content:"yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,"
+        },
+    ]
     return <div > 
-        
+        <Accordion items={items} />
         </div>
 }
 
