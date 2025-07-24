9bb5071e95b61e785a779d295f048242bffb3fab
Hossein <hosseinbehvandi80@gmail.com>
2025-07-25 00:04:56 +0330

    make handle click for both collaps and toggle

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import { FaAngleDown ,FaAngleLeft  } from "react-icons/fa";
 function Accordion({items}){
-    const [expendedIndex , setExpendedIndex] = useState(5)
+    const [expendedIndex , setExpendedIndex] = useState(-1)
+    const handleSubmit=(nextIndex)=>{
+        if (expendedIndex===nextIndex){
+            setExpendedIndex(-1);
+        }
+        else{
+            setExpendedIndex(nextIndex);
+        }
+    }
     const renderedAccordion= items.map((item , index)=>{
         const isExpended = index === expendedIndex
         const icon = <span className="text-3xl">
+            
             {isExpended ? <FaAngleDown/> : <FaAngleLeft />}
         </span>
         return <div key={item.id}>
-            <div onClick={()=>setExpendedIndex(index)} className="cursor-pointer flex items-center justify-between p-3 bg-gray-50 border-b ">
+            <div onClick={()=>handleSubmit(index)} className="cursor-pointer flex items-center justify-between p-3 bg-gray-50 border-b ">
                 {item.lable}
                 {icon}
                 </div>
