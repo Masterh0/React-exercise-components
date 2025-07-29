aa3acf6b7c96ec5996253f00347d734f5576f89f
Hossein <hosseinbehvandi80@gmail.com>
2025-07-30 02:03:38 +0330

    create Accordion page js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import Accordion from "./components/Accordion";
+
 function App(){
-    const items=[
-        {
-            id:"gjwuer9f",
-            lable:"can use react in this project",
-            content:"yes you can use react as you want in any project,yes you can use react as you want in any project,yes you can use react as you want in any project,yes you can use react as you want in any project,"
-        },
-        {
-            id:"qwepigherh9g",
-            lable:"can use css in this project",
-            content:"yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,"
-        },
-        {
-            id:"ewrg;pkjhweg",
-            lable:"can use js in this project",
-            content:"yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,"
-        },
-    ]
-    return <div > 
-        <Accordion items={items} />
-        </div>
+    return <div>
+        app
+    </div>
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,25 @@
+import Accordion from "../components/Accordion";
+function AccordionPage(){
+    const items=[
+        {
+            id:"gjwuer9f",
+            lable:"can use react in this project",
+            content:"yes you can use react as you want in any project,yes you can use react as you want in any project,yes you can use react as you want in any project,yes you can use react as you want in any project,"
+        },
+        {
+            id:"qwepigherh9g",
+            lable:"can use css in this project",
+            content:"yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,yes you can use css as you want in any project,"
+        },
+        {
+            id:"ewrg;pkjhweg",
+            lable:"can use js in this project",
+            content:"yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,yes you can use js as you want in any project,"
+        },
+    ]
+    return <div > 
+        <Accordion items={items} />
+        </div>
+}
+
+export default AccordionPage;
