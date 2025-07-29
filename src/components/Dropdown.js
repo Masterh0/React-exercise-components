6210bcb90bac5ac348a821487a103fd323b8b9ce
Hossein <hosseinbehvandi80@gmail.com>
2025-07-30 02:20:18 +0330

    handle click for open and close options

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,16 @@
-function Dropdown(){
-    return <div>drop down .....</div>
+import { useState } from "react"
+function Dropdown({options}){
+    const [isOpen , setIsOpen] = useState(false)
+    const handleSubmit=()=>{
+        setIsOpen(!isOpen)
+    }
+    const renderedOptions = options.map((option)=>{
+        return <div key={option.value}>
+            {option.lable}
+        </div>
+    })
+    return <div onClick={handleSubmit}>drop down .....
+        {isOpen && <div>{renderedOptions}</div>}
+    </div>
 }   
 export default Dropdown;
