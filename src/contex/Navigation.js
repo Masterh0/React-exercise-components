eb95ffaf06efcbcb2c9b1f862f99d84fdeabcf2a
Hossein <hosseinbehvandi80@gmail.com>
2025-08-01 17:37:25 +0330

    fully create navigat fin in navigationContex

--- src/contex/Navigation.js
+++ src/contex/Navigation.js
@@ -1,9 +1,23 @@
-import { createContext } from "react";
+import { createContext , useState , useEffect } from "react";
 
 const NavigationContex = createContext();
 function NavigationProvider ({children}){
+    const [path , setPath] = useState(window.location.current);
+    useEffect(()=>{
+       const handler = ()=>{
+        setPath(window.location.pathname)
+        };
+        document.addEventListener("popstate", handler);
+        return()=>{
+            document.removeEventListener("popstate", handler);
+        }
+    },[])
+    const navigate=(to)=>{
+        window.history.pushState({}, '' , to);
+        setPath(to);
+    }
         return(
-            <NavigationContex.Provider  value={""}>
+            <NavigationContex.Provider  value={{path , navigate}}>
                 {children}
             </NavigationContex.Provider>
     )
