025527af4bc888d8b30968caf44457625b4a4055
Hossein <hosseinbehvandi80@gmail.com>
2025-08-04 19:35:29 +0330

    create a portal with react dom  for modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,13 @@
-function Modal (){
-    return <div className="absolute inset-0 bg-gray-300 opacity-80">
-        <div className="absolute inset-40 bg-white flex items-center justify-center rounded">
-            <p>its a modal</p>
-        </div>
-    </div>
+import ReactDom from "react-dom"
+function Modal() {
+    return ReactDom.createPortal(
+        <div className="absolute inset-0 bg-gray-300 opacity-80">
+      <div className="absolute inset-40 bg-white flex items-center justify-center rounded">
+        <p>its a modal</p>
+      </div>
+    </div>,
+    document.querySelector(".modal-container")
+    );
+
 }
 export default Modal;
