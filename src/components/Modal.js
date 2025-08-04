69141aa2413b2fa4fd5f21f8c477a3620b0486a8
Hossein <hosseinbehvandi80@gmail.com>
2025-08-04 19:13:55 +0330

    a way to create a modal right center of the page by tailwind code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,8 @@
 function Modal (){
-    return <div>Modal</div>
+    return <div className="absolute inset-0 bg-gray-300 opacity-80">
+        <div className="absolute inset-40 bg-white flex items-center justify-center rounded">
+            <p>its a modal</p>
+        </div>
+    </div>
 }
 export default Modal;
