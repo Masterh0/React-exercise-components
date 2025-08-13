7d3572c8c4009729dc3da887ab7c376728d25e1c
Hossein <hosseinbehvandi80@gmail.com>
2025-08-13 21:25:55 +0330

    add this props to modal comp

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,10 @@
 import ReactDom from "react-dom"
-function Modal({onClose}) {
+function Modal({onClose , children , actionBar}) {
     return ReactDom.createPortal(
         <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80">
       <div className="absolute inset-40 bg-white flex items-center justify-center rounded">
-        <p>its a modal</p>
+        {children}
+        {actionBar}
       </div>
     </div>,
     document.querySelector(".modal-container")
