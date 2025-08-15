fa5c32dba0d357d12d4ed4a1ed7ac9ec1f779408
Hossein <hosseinbehvandi80@gmail.com>
2025-08-15 17:24:09 +0330

    a little debug with styling using js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import ReactDom from "react-dom";
+
 function Modal({ onClose, children, actionBar }) {
+  useEffect(()=>{
+    document.body.classList.add("overflow-hidden")
+    return ()=>{
+      document.body.classList.remove("overflow-hidden")
+    }
+  },[])
   return ReactDom.createPortal(
     <div>
-    <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"> </div>
-      <div className="absolute inset-40 bg-white flex items-center justify-center rounded">
-        <div className="flex flex-col justify-between h-full">
+    <div onClick={onClose} className="fixed inset-0 bg-gray-300 opacity-80"> </div>
+      <div className="fixed inset-40 bg-white flex items-center justify-center rounded">
+        <div className="flex  flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
         </div>
