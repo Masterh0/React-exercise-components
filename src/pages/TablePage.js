2d53e2b0a42a8d7e14620f75582fee25755696ea
Hossein <hosseinbehvandi80@gmail.com>
2025-08-15 17:45:51 +0330

    create a data for table comp

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,11 @@
-function TablePage(){
-    return <div>table</div>   
+import Table from "../components/Table";
+const data = [
+  { name: "red apple", color: "bg-red-500", score: 3 },
+  { name: "banana", color: "bg-yellow-500", score: 5 },
+  { name: "orange", color: "bg-orange-500", score: 2 },
+  { name: "grape", color: "bg-purpule-500", score: 4 },
+];
+function TablePage() {
+  return <Table date={data}/>;
 }
 export default TablePage;
